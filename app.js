d6cb101b0f6c533ce8a5cf0ac4b1835f6bf2815e
Simonini <simonini_thomas@outlook.fr>
2016-07-03 09:43:36 +0200

    Set up the environement

---
+++ app.js
@@ -0,0 +1,40 @@
+/**********************************************************************************************************
+ *                                                      Sir PlayABot
+ *                                                      @author: SIMONINI Thomas
+ *                                                      @email: simonini_thomas@outlook.fr
+ *                                                      All Rights Reserved
+ * *******************************************************************************************************/
+var express = require('express');
+var parser = require('body-parser');
+var request = require('request');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Process application/json
+app.use(bodyParser.json());
+
+// Index route
+app.get('/', function(req, res) {
+    res.send("Hello world! I'm a bot");
+});
+
+// Facebook verification
+app.get('/webhook/', function(req, res){
+    if(req.query['hub.verify_token'] === 'hello_sir'){
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token');
+});
+
+// Turn on the server
+app.listen(app.get('port'), function(){
+    console.log('Running on port', app.get('port'))
+});
+
+
+
